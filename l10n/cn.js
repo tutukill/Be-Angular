1070523f5f1cf1a52389eb5fc228864111a0bb6c
LIUQIANG <tutukill@gmail.com>
2016-03-30 08:38:24 +0800

    1.0

---
+++ l10n/cn.js
@@ -0,0 +1,72 @@
+{
+
+"header" : {
+  "navbar" : {
+    "UPLOAD" : "上传",
+    "new" : {
+      "NEW" : "更新",
+      "PROJECT" : "项目",
+      "TASK" : "课题",
+      "USER" : "用户",
+      "EMAIL" : "邮件"
+    },
+    "NOTIFICATIONS" : "通知"
+  }
+},
+"aside" : {
+  "nav" : {
+    "HEADER" : "Navigation",
+    "DASHBOARD" : "Dashboard",
+    "CALENDAR" : "日历",
+    "EMAIL" : "Email",
+    "WIDGETS" : "Widgets",
+    "components" : {
+      "COMPONENTS" : "组件",
+      "ui_kits" : {
+        "UI_KITS" : "UI Kits",
+        "BUTTONS" : "Buttons",
+        "ICONS" : "图标",
+        "GRID" : "网格",
+        "BOOTSTRAP" : "Bootstrap",
+        "SORTABLE" : "Sortable",
+        "PORTLET" : "Portlet",
+        "TIMELINE" : "时间轴",
+        "VECTOR_MAP" : "矢量地图"
+      },
+      "table" : {
+        "TABLE" : "表格",
+        "TABLE_STATIC" : "静态表格",
+        "DATATABLE" : "动态表格",
+        "FOOTABLE" : "Footable"
+      },
+      "form" : {
+        "FORM" : "表单",
+        "FORM_ELEMENTS" : "表单元素",
+        "FORM_VALIDATION" : "表单验证",
+        "FORM_WIZARD" : "Form wizard"
+      },
+      "CHART" : "图表",
+      "pages" : {
+        "PAGES" : "Pages",
+        "PROFILE" : "Profile",
+        "POST" : "Post",
+        "SEARCH" : "搜索",
+        "INVOICE" : "Invoice",
+        "LOCK_SCREEN" : "锁屏",
+        "SIGNIN" : "登录",
+        "SIGNUP" : "注册",
+        "FORGOT_PASSWORD" : "忘记密码",
+        "404" : "404"
+      }
+    },
+    "your_stuff" : {
+      "YOUR_STUFF": "Your Stuff",
+      "PROFILE" : "Profile",
+      "DOCUMENTS" : "Documents"
+    }
+  },
+  "MILESTONE" : "Milestone",
+  "RELEASE" : "Release"
+}
+
+}
